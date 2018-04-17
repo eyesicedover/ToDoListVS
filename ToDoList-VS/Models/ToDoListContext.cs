cdbb7a83eaf352266efcf63bd1e2195d1a583fa1
Michael Albers and Stephanie Faber <michael@>
2018-04-17 10:01:58 -0700

    add files to start migration

--- ToDoList-VS/Models/ToDoListContext.cs
+++ ToDoList-VS/Models/ToDoListContext.cs
@@ -5,11 +5,25 @@ namespace ToDoListVS.Models
 {
     public class ToDoListContext : DbContext
     {
+        public ToDoListContext()
+        {
+        }
+
         public virtual DbSet<Category> Categories { get; set; }
         public virtual DbSet<Item> Items { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
             => optionsBuilder
                 .UseMySql(@"Server=localhost;Port=8889;database=todolist;uid=root;pwd=root;");
+        
+        public ToDoListContext(DbContextOptions<ToDoListContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
     }
 }
